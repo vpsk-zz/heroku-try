68da049ab8065fe31d7ba1efaf45b1cf7d065855
virendra <vpsk05@gmail.com>
2017-03-15 21:06:44 +0530

    code updated again

--- server.js
+++ server.js
@@ -1,23 +1,80 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+//initialize mongoose schemas
+require('./models/models');
+var index = require('./routes/index');
+var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
+var mongoose = require('mongoose');
+var user = "vpsk05";
+var pass = "Vpsk05@g"                         //add for Mongo support
+process.env.MONGOLAB_URI = "mongodb://vpsk05:vpsk0505@ds119370.mlab.com:19370/test-chirp";
+mongoose.connect(process.env.MONGOLAB_URI);              //connect to Mongo
 var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
+//mongodb://example:example@ds053312.mongolab.com:53312/todolist
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// set the home page route
-app.get('/', function(req, res) {
+app.use('/', index);
+app.use('/auth', authenticate);
+app.use('/api', api);
 
-	// ejs render automatically looks in the views folder
-	res.render('index');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+
+module.exports = app;
